8c6d97e0ce2510076ecc38c4e788cc444397f86f
omacneil <dan@omacneil.org>
2023-06-30 16:23:15 -0400

    now ignore/clean dists/ sub dir

--- clean.sh
+++ clean.sh
@@ -4,9 +4,10 @@
 # cached byte code / build / lint / test tmp files
 # so grepping for stuff is easier.
 
-# NOTE: vscode and various linters wouldn't find libraries 
+# NOTE: vscode and various linters wouldn't find libraries
 #       until you re-run poetry install
 
+rm dist -rf
 rm ~/git/diffant/.venv  -rf
 rm ~/git/diffant/.tox   -rf
 rm .mypy_cache          -rf
